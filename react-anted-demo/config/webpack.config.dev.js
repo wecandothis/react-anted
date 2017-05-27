068fbe2f112c3d31e18692a57717bd3363f612c6
feng <490884308@qq.com>
2017-05-27 16:42:31 +0800

    下载一些需要的依赖

--- react-anted-demo/config/webpack.config.dev.js
+++ react-anted-demo/config/webpack.config.dev.js
@@ -167,54 +167,55 @@ module.exports = {
         include: paths.appSrc,
         loader: require.resolve('babel-loader'),
         options: {
-          
+           plugins:[ 
+     ['import', { libraryName: 'antd', style: true }]
+   ],
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
           // directory for faster rebuilds.
           cacheDirectory: true,
-        },
-       query: {
-       plugins: [
-          ['import', [{ libraryName: 'antd', style: true }]], 
-          ],
-      },
+        }
+         
+  },
+        
+  
       // "postcss" loader applies autoprefixer to our CSS.
       // "css" loader resolves paths in CSS and adds assets as dependencies.
       // "style" loader turns CSS into JS modules that inject <style> tags.
       // In production, we use a plugin to extract that CSS to a file, but
       // in development "style" loader enables hot editing of CSS.
-      {
-+   test: /\.less$/,
-+   use: [
-+     require.resolve('style-loader'),
-+     require.resolve('css-loader'),
-+     {
-+       loader: require.resolve('postcss-loader'),
-+       options: {
-+         ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
-+         plugins: () => [
-+           require('postcss-flexbugs-fixes'),
-+           autoprefixer({
-+             browsers: [
-+               '>1%',
-+               'last 4 versions',
-+               'Firefox ESR',
-+               'not ie < 9', // React doesn't support IE8 anyway
-+             ],
-+             flexbox: 'no-2009',
-+           }),
-+         ],
-+       },
-+     },
-+     {
-+       loader: require.resolve('less-loader'),
-+       options: {
-+         modifyVars: { "@primary-color": "#1DA57A" },
-+       },
-+     },
-+   ],
-+ },
-],
+ // 解析 less 文件，并加入变量覆盖配置
+ {
+   test: /\.less$/,
+   use: [
+    require.resolve('style-loader'),
+    require.resolve('css-loader'),
+     {
+       loader: require.resolve('postcss-loader'),
+       options: {
+         ident: 'postcss', // https://webpack.js.org/guides/migrating/#complex-options
+         plugins: () => [
+           require('postcss-flexbugs-fixes'),
+          autoprefixer({
+             browsers: [
+               '>1%',
+               'last 4 versions',
+               'Firefox ESR',
+               'not ie < 9', // React doesn't support IE8 anyway
+             ],
+             flexbox: 'no-2009',
+           }),
+         ],
+       },
+     },
+     {
+       loader: require.resolve('less-loader'),
+       options: {
+         modifyVars: { "@primary-color": "#1DA57A" },
+       },
+     },
+   ],
+ },
       {
         test: /\.css$/,
         use: [
